731cc4afb5ba251991760e147344871735e6cdd7
Andrew Morris <andrew.morris.crypto@gmail.com>
2023-01-24 20:54:18 +0000

    add number game and list items

--- app.js
+++ app.js
@@ -37,3 +37,45 @@ function giveResponse(question, answerGiven, answerExpected) {
     alert("Incorrect! " + message);
   }
 }
+
+function numberGame() {
+  const promptMsg = "Can you guess what integer I'm thinking of between 1 and 100 in four our fewer attempts? What's your first guess?";
+  const tryAgainMsg = ".  Try agian:";
+  const lowerMsg = "That's too high";
+  const higherMsg = "That's too low";
+  const nanMsg = "That not an integer"
+  const successMsg = "We done, that's correct!";
+  const failMsg = "Sorry, you ran out of attepmts. Better luck next time!";
+  const myInt = 51;
+  const maxAttempts = 4;
+  let answer;
+  let message = promptMsg;
+
+  for (let i = 0; i < maxAttempts; i++) {
+    answer = parseInt(prompt(message + " (1..100)"));
+    console.log("answer = " + answer);
+    console.log(answer == NaN);
+    console.log(answer === NaN);
+
+
+    if (isNaN(answer)) {
+      message = nanMsg;
+    } else if (myInt < answer) {
+      message = lowerMsg;
+    } else if (answer < myInt) {
+      message = higherMsg;
+    } else {
+      message = successMsg;
+      break;
+    }
+
+    if (i < maxAttempts - 1) {
+      message += ". Attempt " + (i + 1) + " of " + maxAttempts + tryAgainMsg;
+    } else {
+      message += failMsg;
+
+    }
+  }
+  alert(message);
+}
+
